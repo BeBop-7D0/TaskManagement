8b3635b1ee45239ee024b4370dadfe6b55810213
@denis.muhaev <denis.muhaev@logx.ru>
2026-08-24 17:42:45 +0300

    Добавил тесты для Task

--- tests/domain_models/test_task.py
+++ tests/domain_models/test_task.py
@@ -98,8 +98,13 @@ def test_task_creates_valid_value():
     task = Task.create(
         **REQUIRED_PARAMS
     )
-    assert isinstance(task, Task)
     assert task.id is not None
+    assert task.title == REQUIRED_PARAMS["title"]
+    assert task.description == REQUIRED_PARAMS["description"]
+    assert task.creator_id == REQUIRED_PARAMS["creator_id"]
+    assert task.executor_id == REQUIRED_PARAMS["executor_id"]
+    assert task.project_id == REQUIRED_PARAMS["project_id"]
+    assert task.status == REQUIRED_PARAMS["status"]
 
 
 def test_task_creates_with_correct_deadline_value():
@@ -145,81 +150,82 @@ def test_task_creates_with_wrong_estimated_time():
              estimated_minutes=-1
         )
 
-simple_task = Task.create(
-        **REQUIRED_PARAMS
-    )
-def test_task_change_title_correct_value():
+@pytest.fixture
+def simple_task():
+    return Task.create(**REQUIRED_PARAMS)
+
+
+def test_task_change_title_correct_value(simple_task):
     simple_task.change_title("new title")
     assert simple_task.title == "new title"
 
 
-def test_task_change_title_wrong_value():
+def test_task_change_title_wrong_value(simple_task):
     with pytest.raises(ValueError):
         simple_task.change_title("      ")
 
 
-def test_task_change_description_correct_value():
-    simple_task.change_title("new description")
-    assert simple_task.title == "new description"
+def test_task_change_description_correct_value(simple_task):
+    simple_task.change_description("new description")
+    assert simple_task.description == "new description"
 
 
-def test_task_change_description_wrong_value():
+def test_task_change_description_wrong_value(simple_task):
     with pytest.raises(ValueError):
-        simple_task.change_title("      ")
+        simple_task.change_description("      ")
 
 
-def test_task_change_executor_correct_value():
-    simple_task.change_title("new executor")
-    assert simple_task.title == "new executor"
+def test_task_change_executor_correct_value(simple_task):
+    simple_task.change_executor("new executor")
+    assert simple_task.executor_id == "new executor"
 
 
-def test_task_change_executor_wrong_value():
+def test_task_change_executor_wrong_value(simple_task):
     with pytest.raises(ValueError):
-        simple_task.change_title("      ")
+        simple_task.change_executor("      ")
 
 
-def test_task_change_status_correct_value():
-    simple_task.change_title("new status")
-    assert simple_task.title == "new status"
+def test_task_change_status_correct_value(simple_task):
+    simple_task.change_status("new status")
+    assert simple_task.status == "new status"
 
 
-def test_task_change_status_wrong_value():
+def test_task_change_status_wrong_value(simple_task):
     with pytest.raises(ValueError):
-        simple_task.change_title("      ")
+        simple_task.change_status("      ")
 
 
-def test_task_set_deadline_correct_value():
+def test_task_set_deadline_correct_value(simple_task):
     dt = datetime.now(tz=timezone.utc) + timedelta(days=1)
     simple_task.set_deadline(dt)
 
-    assert simple_task is not None
     assert simple_task.deadline == Deadline(deadline=dt)
 
 
-def test_task_set_deadline_with_past_value():
+def test_task_set_deadline_with_past_value(simple_task):
     dt = datetime.now(tz=timezone.utc) - timedelta(days=1)
     with pytest.raises(ValueError):
         simple_task.set_deadline(dt)
 
 
-def test_task_set_estimated_time_correct_value():
+def test_task_set_estimated_time_correct_value(simple_task):
     simple_task.set_estimated_time(hours=12, minutes=30)
 
     assert simple_task.estimated_time == TimeRecord(hours=12, minutes=30)
 
 
-def test_task_set_estimated_time_with_wrong_value():
+def test_task_set_estimated_time_with_wrong_value(simple_task):
     with pytest.raises(ValueError):
         simple_task.set_estimated_time(hours=1.2, minutes=30)
 
 
-def test_task_add_spend_time_correct_value():
+def test_task_add_spend_time_correct_value(simple_task):
     simple_task.add_spend_time(hours=1, minutes=30)
     simple_task.add_spend_time(hours=1, minutes=30)
 
     assert simple_task.spent_time == TimeRecord(hours=3, minutes=00)
 
 
-def test_task_add_spend_time_with_wrong_value():
+def test_task_add_spend_time_with_wrong_value(simple_task):
     with pytest.raises(ValueError):
         simple_task.add_spend_time(hours=-3, minutes=00)
